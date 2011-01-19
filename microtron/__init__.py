b596ab33263cd93dc407fe08add3552f532ed6e1
Andrew McCollum <amccollum+github@gmail.com>
2011-01-19 10:30:01 -0500

    better handling of string-equivalent values

--- microtron/__init__.py
+++ microtron/__init__.py
@@ -51,7 +51,8 @@ class Parser(object):
             prop_many = prop.attrib['many'] if 'many' in prop.attrib else False
             prop_couldbe = prop.attrib['couldbe'].split('|') if 'couldbe' in prop.attrib else []
             prop_values = set(prop.attrib['values'].split(',')) if 'values' in prop.attrib else None
-
+            prop_separator = prop.attrib['separator'] if 'separator' in prop.attrib else ""
+            
             # Select all properties, but exclude nested properties
             prop_expr = 'descendant-or-self::*[contains(concat(" ", normalize-space(@%s), " "), " %s ")]' % (prop_attr, prop_name)
             parent_expr = 'ancestor::*[contains(concat(" ", normalize-space(@class), " "), " %s ")]' % format.tag
@@ -62,7 +63,7 @@ class Parser(object):
 
             if prop_many == 'many':
                 values = []
-            elif prop_many == "manyasone":
+            elif prop_many == 'manyasone':
                 values = ""
 
             for prop_node in prop_nodes:
@@ -81,6 +82,7 @@ class Parser(object):
                         except:
                             pass
 
+                    # Check if this property is a compound property
                     if len(prop):
                         try:
                             prop_result = self._parse_node(prop_node, prop)
@@ -126,6 +128,7 @@ class Parser(object):
                             value['date'] = isodate.parse_date(self._parse_value(prop_node))
 
                         else:
+                            # Try to parse this property as a sub-format
                             results = self.parse_format(prop_type, prop_node)
                             if results and len(results[0]) > 1:
                                 value = results[0]
@@ -139,18 +142,26 @@ class Parser(object):
                     else:
                         value = self._parse_value(prop_node)
 
-                if self.strict and prop_values and value.lower() not in prop_values:
+                # Convert the value to a string (if it isn't one already)
+                if isinstance(value, basestring):
+                    value_text = value
+                elif 'text' in value:
+                    value_text = value['text']
+                else:
+                    value_text = ""
+
+                if self.strict and prop_values and value_text.lower() not in prop_values:
                     raise ParseError("Invalid value for property %s: %s" % (prop_name, value))
 
                 if prop_many == 'many':
                     values.append(value)
 
                 elif prop_many == 'manyasone':
-                    if isinstance(value, basestring):
-                        if values and 'separator' in prop.attrib:
-                            values += prop.attrib['separator']
+                    if value_text:
+                        if values and prop_separator:
+                            values += prop_separator
 
-                        values += value
+                        values += value_text
 
                 else:
                     result[prop_name] = value
@@ -165,6 +176,7 @@ class Parser(object):
         value_expr = 'descendant::*[contains(concat(" ", normalize-space(@class), " "), " value ")]'
         value_nodes = node.xpath(value_expr)
 
+        # Check for various methods to override the tag text
         if value_nodes:
             return " ".join(self._parse_text(value_node) for value_node in value_nodes)
 
